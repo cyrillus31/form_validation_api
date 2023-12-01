2d69ec5c6c3b89c74e00d2f13713b8f645bf0860
Kirill Fedtsov <kirill@Air-M1.local>
2023-12-01 07:27:11 +0300

    mongo works correctly

--- app/database/mongo.py
+++ app/database/mongo.py
@@ -3,14 +3,14 @@ from pymongo import MongoClient
 from database.forms import forms
 
 
-client: MongoClient = MongoClient()
+client: MongoClient = MongoClient(host="mongo")
 
-db = client["forms"]
+db = client["forms_database"]
 amount_forms = db.forms.count_documents({})
 if not amount_forms:
     db.forms.insert_many(forms)
 
 
 def find_forms_by_fields(*args) -> list[dict]:
-    result = db.forms.find({f"form_fields.{field_name}": {"$exists": "true"} for field_name in args})
-    return [form for form in result] 
+    result = db.forms.find()
+    return [form for form in result if set(form["form_fields"].keys()).issubset(set(args))] 
